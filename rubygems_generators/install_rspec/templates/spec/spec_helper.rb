6ca66d2ecce92f6ac561c90cb7a14f27facaf5c7
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2009-05-07 13:17:14 +0200

    (slightly) better spec helper

--- rubygems_generators/install_rspec/templates/spec/spec_helper.rb
+++ rubygems_generators/install_rspec/templates/spec/spec_helper.rb
@@ -8,3 +8,14 @@ end
 
 require File.dirname(__FILE__) + '/../config/boot'
 require '<%= gem_name %>'
+
+Spec::Runner.configure do |config|
+  # == Mock Framework
+  #
+  # RSpec uses it's own mocking framework by default. If you prefer to
+  # use mocha, flexmock or RR, uncomment the appropriate line:
+  #
+  config.mock_with :mocha
+  # config.mock_with :flexmock
+  # config.mock_with :rr
+end
