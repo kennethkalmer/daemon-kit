43aca8a2b5246e43815f06f3b7fad1c11ac357c0
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2009-05-08 15:43:51 +0200

    Typo in generator

--- lib/daemon_kit.rb
+++ lib/daemon_kit.rb
@@ -4,7 +4,7 @@ $:.unshift(File.dirname(__FILE__)) unless
 require 'rubygems'
 
 module DaemonKit
-  VERSION = '0.1.5'
+  VERSION = '0.1.5.1'
 
   autoload :Initializer, 'daemon_kit/initializer'
   autoload :Application, 'daemon_kit/application'
