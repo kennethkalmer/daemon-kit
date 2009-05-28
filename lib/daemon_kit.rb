21edf96644bb8e92049fc493dd18095b6232c316
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2009-05-28 03:15:22 +0200

    Bump github gem to 0.1.7.1

--- lib/daemon_kit.rb
+++ lib/daemon_kit.rb
@@ -4,7 +4,7 @@ $:.unshift(File.dirname(__FILE__)) unless
 require 'rubygems'
 
 module DaemonKit
-  VERSION = '0.1.7'
+  VERSION = '0.1.7.1'
 
   autoload :Initializer, 'daemon_kit/initializer'
   autoload :Application, 'daemon_kit/application'
