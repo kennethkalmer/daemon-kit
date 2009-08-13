b0c66bab0328c9748025ca840445d7ec21dd928f
Brian P O'Rourke <bpo@somnambulance.net>
2009-08-14 07:20:08 +0800

    Adding the DaemonNotFound exception.
    It's pretty weird when you can't find the exception that tells you you
    can't find the daemon.
    
    Signed-off-by: Kenneth Kalmer <kenneth.kalmer@gmail.com>

--- lib/daemon_kit/exceptions.rb
+++ lib/daemon_kit/exceptions.rb
@@ -5,4 +5,11 @@ module DaemonKit
 
   # Raised when no class is registered to process a ruote workitem
   class MissingParticipant < Exception; end
+
+  # Raised when the daemon itself cannot be found.
+  class DaemonNotFound < Exception
+    def initialize( file )
+      super "No daemon found at the path '#{file}'"
+    end
+  end
 end
