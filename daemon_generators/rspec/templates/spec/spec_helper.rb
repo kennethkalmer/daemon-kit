8b76e31f521d3629b1caf20c89279f63f2ccf8ff
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2009-09-17 00:34:37 +0200

    Mocha should be optional for rspec

--- daemon_generators/rspec/templates/spec/spec_helper.rb
+++ daemon_generators/rspec/templates/spec/spec_helper.rb
@@ -1,3 +1,5 @@
+DAEMON_ENV = 'test' unless defined?( DAEMON_ENV )
+
 begin
   require 'spec'
 rescue LoadError
@@ -15,7 +17,7 @@ Spec::Runner.configure do |config|
   # RSpec uses it's own mocking framework by default. If you prefer to
   # use mocha, flexmock or RR, uncomment the appropriate line:
   #
-  config.mock_with :mocha
+  # config.mock_with :mocha
   # config.mock_with :flexmock
   # config.mock_with :rr
 end
