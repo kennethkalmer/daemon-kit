532a5489ac32181f54f71af49579bc4ecd2b4552
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2009-11-22 21:30:47 +0200

    Proof of concept blather

--- lib/daemon_kit/xmpp.rb
+++ lib/daemon_kit/xmpp.rb
@@ -1,3 +1,6 @@
+require 'blather/client/client'
+require 'blather/client/dsl'
+
 module DaemonKit
   # Thin wrapper around the blather DSL
   class XMPP
@@ -6,18 +9,20 @@ module DaemonKit
     class << self
 
       def run( &block )
-        DaemonKit::EM.run
+        DaemonKit.trap('INT') { ::EM.stop }
+        DaemonKit.trap('TERM') { ::EM.stop }
 
-        xmpp = new
+        DaemonKit::EM.run {
 
-        xmpp.instance_eval( &block )
-
-        xmpp.run
+          xmpp = new
+          xmpp.instance_eval( &block )
+          xmpp.run
+        }
       end
     end
 
     def initialize
-      @config = DaemonKit::Config.load('jabber')
+      @config = DaemonKit::Config.load('xmpp')
 
       jid = if @config.resource
         "#{@config.jabber_id}/#{@config.resource}"
@@ -26,6 +31,37 @@ module DaemonKit
       end
 
       setup jid, @config.password
+
+      when_ready { configure_roster! }
+    end
+
+    def configure_roster!
+      DaemonKit.logger.debug 'Configuring roster'
+
+      #my_roster.each do |item|
+      #  p [ :roster_item, item ]
+      #  unless contacts.include?( item.jid )
+      #    item.subscription = :none
+      #    next
+      #  end
+
+      #  if @config['masters'].include?( item.jid )
+      #    item.subscription = :both
+      #    next
+      #  end
+
+      #  if @config['supporters'].include?( item.jid )
+      #    item.subscription = :from
+      #  end
+      #end
+    end
+
+    def contacts
+      @config['masters'] + @config['supporters']
+    end
+
+    def run
+      client.run
     end
   end
 end
