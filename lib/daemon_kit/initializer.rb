6e493939d279a2dc4db636ca636adf4bb64323f6
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2010-01-05 13:07:30 +0200

    Updated list of commands we ignore

--- lib/daemon_kit/initializer.rb
+++ lib/daemon_kit/initializer.rb
@@ -384,7 +384,7 @@ module DaemonKit
     # arguments to be parsed cause they will interfere with the
     # script encapsulating DaemonKit, like capistrano
     def own_args?
-      ![ 'cap' ].include?( File.basename( $0 ) )
+      !%w( rake cap spec cucumber ).include?( File.basename( $0 ) )
     end
   end
 
