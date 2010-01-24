89bc142441b69ba0008501f1d5de9e6ee1ab6efb
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2010-01-24 23:13:29 +0200

    Converted capistrano generator to Thor

--- daemon_generators/deploy_capistrano/templates/config/deploy/staging.rb
+++
@@ -1,6 +0,0 @@
-#set :deploy_to, "/svc/<%= name %>" # defaults to "/u/apps/#{application}"
-#set :user, "<% name %>"            # defaults to the currently logged in user
-set :daemon_env, 'staging'
-
-set :domain, 'example.com'
-server domain
