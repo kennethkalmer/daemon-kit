cce6cdca3010001ed1dc336157f383398867a334
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2010-01-24 22:55:23 +0200

    Converted cron generator to Thor

--- lib/daemon_kit/generators.rb
+++ lib/daemon_kit/generators.rb
@@ -5,5 +5,6 @@ module DaemonKit
     autoload :AppGenerator,      'generators/daemon_kit/app/app_generator'
     autoload :CucumberGenerator, 'generators/daemon_kit/cucumber/cucumber_generator'
     autoload :AmqpGenerator,     'generators/daemon_kit/amqp/amqp_generator'
+    autoload :CronGenerator,     'generators/daemon_kit/cron/cron_generator'
   end
 end
---
+++ lib/generators/daemon_kit/cron/cron_generator.rb
@@ -0,0 +1,24 @@
+module DaemonKit
+  module Generators
+    class CronGenerator < Base
+
+      def update_gemfile
+        append_file 'Gemfile', "gem 'rufus-scheduler', '>= 2.0.3'"
+      end
+
+      def create_initializers
+        directory 'config'
+      end
+
+      def create_daemon
+        directory 'libexec'
+      end
+
+      protected
+
+      def self.source_root
+        File.expand_path( File.join( File.dirname(__FILE__), 'templates') )
+      end
+    end
+  end
+end
