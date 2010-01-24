48ec1ea04da31d863c5dabefaf2c425989b2faaf
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2010-01-25 00:36:12 +0200

    Refactoed script/{generate|destroy} for thor compat

--- lib/daemon_kit/generators.rb
+++ lib/daemon_kit/generators.rb
@@ -1,3 +1,6 @@
+# TODO: Don't always depend on bundled thor
+$:.unshift File.dirname(__FILE__) + '/vendor/thor-0.12.3/lib'
+
 require 'daemon_kit/generators/base'
 
 module DaemonKit
@@ -12,5 +15,53 @@ module DaemonKit
     autoload :TestUnitGenerator,    'generators/daemon_kit/test_unit/test_unit_generator'
     autoload :RuoteGenerator,       'generators/daemon_kit/ruote/ruote_generator'
     autoload :XmppGenerator,        'generators/daemon_kit/xmpp/xmpp_generator'
+
+    class << self
+
+      def configure!
+      end
+
+      def invoke( namespace, args = ARGV, config = {} )
+        klass_name = constants.detect do |sym|
+          klass = const_get( sym )
+          klass.respond_to?( :namespace ) && klass.namespace == namespace
+        end
+
+        if klass_name.nil?
+          raise Error, "Could not find generator #{namespace}."
+        end
+
+        klass = const_get( klass_name )
+
+        args << '--help' if args.empty? && klass.arguments.any? { |a| a.required? }
+        klass.start( args, config )
+      end
+
+      def help
+        namespaces = constants.inject([]) do |list, sym|
+          unless sym == :Base || sym == :AppGenerator
+            klass = const_get( sym )
+            list << klass.namespace if klass.respond_to?( :namespace )
+          end
+
+          list
+        end
+
+        puts "Usage:"
+        puts "  script/generate GENERATOR [args] [options]"
+        puts
+        puts "General options:"
+        puts "  -h, [--help]     # Print generators options and usage"
+        puts "  -p, [--pretend]  # Run but do not make any changes"
+        puts "  -f, [--force]    # Overwrite files that already exist"
+        puts "  -s, [--skip]     # Skip files that already exist"
+        puts "  -q, [--quiet]    # Supress status output"
+        puts
+        puts "Available generators:"
+
+        namespaces.each { |ns| puts "  " + ns }
+        puts
+      end
+    end
   end
 end
