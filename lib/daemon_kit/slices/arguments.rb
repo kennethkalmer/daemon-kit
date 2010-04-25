d1949db630c3e2501147093d15d632f3682adafc
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2010-04-25 16:52:26 +0200

    Argument parser updates as per master branch

--- lib/daemon_kit/slices/arguments.rb
+++ lib/daemon_kit/slices/arguments.rb
@@ -6,10 +6,6 @@ module DaemonKit
       def arguments
         command, configs, args = ArgumentParser.parse( ARGV.dup )
 
-        # TODO: This isn't correct, we need to determine the calling command
-        # to make sure we don't interfere with things like rake, capistrano,
-        # cucumber, spec, etc...
-        ArgumentParser.parser_available = true
         DaemonKit.arguments = ArgumentParser.new
         DaemonKit.arguments.parse( args )
       end
