c5f83bad9088c91e7455c0f0d28e51567133d7fa
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2010-05-05 00:38:33 +0200

    Getting ready for next rc

--- lib/daemon_kit.rb
+++ lib/daemon_kit.rb
@@ -13,7 +13,7 @@ $:.unshift( File.dirname(__FILE__).to_absolute_path ) unless
   $:.include?( File.dirname(__FILE__).to_absolute_path )
 
 module DaemonKit
-  VERSION = '0.1.8rc2'
+  VERSION = '0.1.8rc3'
 
   autoload :Initializer,    'daemon_kit/initializer'
   autoload :Application,    'daemon_kit/application'
