3be04f724e21da894f0c4ec9988015b267aaa7b3
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2011-05-28 21:26:26 +0200

    Removed deprecated 'mq' requirement in ruote generator

--- lib/generators/daemon_kit/ruote/templates/config/pre-daemonize/ruote.rb
+++ lib/generators/daemon_kit/ruote/templates/config/pre-daemonize/ruote.rb
@@ -7,7 +7,6 @@ end
 
 begin
   require 'amqp'
-  require 'mq'
 rescue LoadError
   $stderr.puts "Missing amqp gem. Please uncomment the amqp section in the Gemfile and run 'bundle install' if you wish to use the AMQP participant/listener pair in ruote"
 end
