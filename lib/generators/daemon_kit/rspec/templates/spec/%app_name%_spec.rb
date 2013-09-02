3ecfe70c440988786274ed71dbf0c50d46e4c135
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2013-09-02 23:16:48 +0200

    Use #pending in example spec instead of #violated

--- lib/generators/daemon_kit/rspec/templates/spec/%app_name%_spec.rb
+++ lib/generators/daemon_kit/rspec/templates/spec/%app_name%_spec.rb
@@ -3,9 +3,9 @@ require File.dirname(__FILE__) + '/spec_helper.rb'
 # Time to add your specs!
 # http://rspec.info/
 describe "Place your specs here" do
-  
+
   it "find this spec in spec directory" do
-    violated "Be sure to write your specs"
+    pending "Be sure to write your specs"
   end
-  
+
 end
