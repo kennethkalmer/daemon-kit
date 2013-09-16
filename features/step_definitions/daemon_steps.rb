8ec558e95e6bdd25aa7946faff0a88c78946ef2c
Kenneth Kalmer <kenneth.kalmer@gmail.com>
2013-09-16 19:47:55 +0200

    Support for generating new daemons with --edge flag

--- features/step_definitions/daemon_steps.rb
+++ features/step_definitions/daemon_steps.rb
@@ -8,3 +8,12 @@ When(/^I accept the conflicts$/) do
   type "a\n"
   #eot
 end
+
+Then(/^the Gemfile should point to edge daemon\-kit$/) do
+  prep_for_fs_check do
+    gemfile = IO.read('Gemfile')
+
+    gemfile.should =~ /^gem 'daemon\-kit', :github => 'kennethkalmer\/daemon\-kit'$/
+    gemfile.should_not =~ /^gem 'daemon\-kit'$/
+  end
+end
--- lib/generators/daemon_kit/app/app_generator.rb
+++ lib/generators/daemon_kit/app/app_generator.rb
@@ -20,6 +20,9 @@ module DaemonKit
       class_option :cucumber, :type => :boolean, :aliases => '-c', :default => false,
                    :desc => "Install cucumber support"
 
+      class_option :edge, :type => :boolean, :default => false,
+                   :desc => "Let the initial Gemfile track kennethkalmer/daemon-kit instead of rubygems"
+
       def initialize( *args )
         super
 
@@ -45,7 +48,7 @@ module DaemonKit
       def create_root_files
         template 'README.tt', 'README'
         copy_file 'Rakefile'
-        copy_file 'Gemfile'
+        template 'Gemfile.tt', 'Gemfile'
       end
 
       def create_bin_files
